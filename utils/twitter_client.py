c75ff32268d914750a297bc4039e96cf4947dd28
AKILESH R <166041192+akileshr07@users.noreply.github.com>
2025-11-29 19:00:37 +0530

    Update twitter_client.py

--- utils/twitter_client.py
+++ utils/twitter_client.py
@@ -1,7 +1,7 @@
 import tweepy
 import os
 
-def get_client():
+def get_client_v2():
     return tweepy.Client(
         consumer_key=os.getenv("X_API_KEY_TW"),
         consumer_secret=os.getenv("X_API_SECRET"),
@@ -9,18 +9,11 @@ def get_client():
         access_token_secret=os.getenv("X_ACCESS_SECRET")
     )
 
-def test_twitter():
-    try:
-        client = get_client()
-        r = client.get_me()
-        return {"ok": True, "me": r.data}
-    except Exception as e:
-        return {"ok": False, "error": str(e), "type": type(e).__name__}
-
 def tweet(text):
     try:
-        client = get_client()
+        client = get_client_v2()
         client.create_tweet(text=text)
         return True
     except Exception as e:
-        return {"error": str(e), "type": type(e).__name__}
+        print("ERROR:", e)
+        return False
